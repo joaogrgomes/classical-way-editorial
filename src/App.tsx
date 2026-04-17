3013baa9434cb0bdc7d6720e287cced1b53ac98d
Joao Gomes <joaogrgomes@gmail.com>
2026-04-16 23:30:54 -0300

    fix: reset scroll position on route change

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import BookReviewsPage from "./pages/BookReviewsPage.tsx";
 import BookResenhaPage from "./pages/BookResenhaPage.tsx";
 import BuscaPage from "./pages/BuscaPage.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import { ScrollToTop } from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -25,6 +26,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/artigos" element={<ArtigosPage />} />
